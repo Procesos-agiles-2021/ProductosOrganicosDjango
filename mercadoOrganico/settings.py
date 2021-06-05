911982a00a82e0fb5984b9af88be6de9dd0a6252
aromero45 <alex4543@hotmail.com>
2021-06-05 10:00:36 -0500

    cambios 2

--- mercadoOrganico/settings.py
+++ mercadoOrganico/settings.py
@@ -96,15 +96,9 @@ WSGI_APPLICATION = 'mercadoOrganico.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-<<<<<<< HEAD
         'NAME': 'productoOrganico',
         'USER': 'aromero45',
         'PASSWORD': 'rome9119',
-=======
-        'NAME': 'ProductosOrganicos2',
-        'USER': 'jdruedaa',
-        'PASSWORD': 'Abc123#',
->>>>>>> dev
         'HOST': 'localhost',
         'PORT': '',
     }
